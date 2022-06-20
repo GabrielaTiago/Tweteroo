70de82d65da57a71267928e8ee62cdff6e0f1edb
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-20 12:42:33 -0300

    Feat: Adding the route for the login page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import express  from "express";
+import express, { response }  from "express";
 
 
 const server = express();
+server.use(express.json());
+
+const users = [];
+
+server.post("/sign-up", (require, response) => {
+    const user = require.body;
+    users.push(user);
+    response.send("OK");
+});
 
 server.listen(5000);
