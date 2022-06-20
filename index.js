e2999b999e067f1de09999f4bbabfb3a6105ba5d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-20 12:54:42 -0300

    Creating the get route to receive the tweets

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import express, { response }  from "express";
-
+import express from "express";
+import cors from "cors";
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 
 const users = [];
+const allTweets = [];
 
 server.post("/sign-up", (require, response) => {
     const user = require.body;
     users.push(user);
     response.send("OK");
+});
+
+server.get("/tweets", (require, response) => {
+    response.send(allTweets);
 });
 
 server.listen(5000);
