bd9bdfcf2d59867d94a7321d3fce235208fb5bdb
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-08 21:16:30 -0300

    Creates the get router for tweets

--- src/routes/tweetsRouter.js
+++ src/routes/tweetsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTweet } from "../controllers/tweetsController.js";
+import { createTweet, getTweets } from "../controllers/tweetsController.js";
 import { validatesSchemas } from "../middlewares/validateSchemas.js";
 
 const tweetRouter = Router();
 
 tweetRouter.post("/tweets", validatesSchemas("tweet"), createTweet);
+tweetRouter.get("/tweets", getTweets)
 
 export { tweetRouter };
